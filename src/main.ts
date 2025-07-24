c3bc0557a51e4ac6f4411288dd2aaf9aa1740c53
OleksandrTech <o.berest2006@gmail.com>
2025-07-24 20:13:53 +0200

    Fix commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+      origin: [
+        'http://localhost:3000', // Your local frontend
+        // 'https://your-frontend-domain.vercel.app' // Your future production frontend
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
