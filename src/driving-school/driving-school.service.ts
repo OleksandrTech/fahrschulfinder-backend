4b5b2bddea8f7d8a02d0a00c6f37a1e96f61d338
OleksandrTech <o.berest2006@gmail.com>
2025-07-26 03:20:07 +0200

    Fix Handling Type Conflicts

--- src/driving-school/driving-school.service.ts
+++ src/driving-school/driving-school.service.ts
@@ -32,9 +32,45 @@ export class DrivingSchoolService {
   async update(id: number, dto: UpdateDrivingSchoolDto) {
     const school = await this.prisma.drivingSchool.findUnique({ where: { id } });
     if (!school) throw new NotFoundException('School not found');
+    
+    // Create a safe data object by filtering out problematic array fields
+    const safeData: any = {};
+    
+    // Only include non-array fields for now
+    if (dto.name !== undefined) safeData.name = dto.name;
+    if (dto.address !== undefined) safeData.address = dto.address;
+    if (dto.phone !== undefined) safeData.phone = dto.phone;
+    if (dto.websiteUrl !== undefined) safeData.websiteUrl = dto.websiteUrl;
+    if (dto.openingHours !== undefined) safeData.openingHours = dto.openingHours;
+    if (dto.serviceArea !== undefined) safeData.serviceArea = dto.serviceArea;
+    if (dto.baseFee !== undefined) safeData.baseFee = dto.baseFee;
+    if (dto.pricePerStandardLesson !== undefined) safeData.pricePerStandardLesson = dto.pricePerStandardLesson;
+    if (dto.pricePerSpecialLesson !== undefined) safeData.pricePerSpecialLesson = dto.pricePerSpecialLesson;
+    if (dto.theoryCourseIncluded !== undefined) safeData.theoryCourseIncluded = dto.theoryCourseIncluded;
+    if (dto.theoryCoursePrice !== undefined) safeData.theoryCoursePrice = dto.theoryCoursePrice;
+    if (dto.firstAidCourseOffered !== undefined) safeData.firstAidCourseOffered = dto.firstAidCourseOffered;
+    if (dto.firstAidCoursePrice !== undefined) safeData.firstAidCoursePrice = dto.firstAidCoursePrice;
+    if (dto.visionTestOffered !== undefined) safeData.visionTestOffered = dto.visionTestOffered;
+    if (dto.visionTestPrice !== undefined) safeData.visionTestPrice = dto.visionTestPrice;
+    if (dto.tuevDekraRegistrationIncluded !== undefined) safeData.tuevDekraRegistrationIncluded = dto.tuevDekraRegistrationIncluded;
+    if (dto.practicalExamFee !== undefined) safeData.practicalExamFee = dto.practicalExamFee;
+    if (dto.theoryExamFee !== undefined) safeData.theoryExamFee = dto.theoryExamFee;
+    if (dto.averageFinalPrice !== undefined) safeData.averageFinalPrice = dto.averageFinalPrice;
+    if (dto.supportForDisabled !== undefined) safeData.supportForDisabled = dto.supportForDisabled;
+    if (dto.hasDigitalClasses !== undefined) safeData.hasDigitalClasses = dto.hasDigitalClasses;
+    if (dto.hasOnlineRegistration !== undefined) safeData.hasOnlineRegistration = dto.hasOnlineRegistration;
+    if (dto.offersIntensiveCourses !== undefined) safeData.offersIntensiveCourses = dto.offersIntensiveCourses;
+    if (dto.specializationInAccompaniedDriving17 !== undefined) safeData.specializationInAccompaniedDriving17 = dto.specializationInAccompaniedDriving17;
+    if (dto.successRate !== undefined) safeData.successRate = dto.successRate;
+    if (dto.logoUrl !== undefined) safeData.logoUrl = dto.logoUrl;
+    if (dto.videoPresentationUrl !== undefined) safeData.videoPresentationUrl = dto.videoPresentationUrl;
+    if (dto.allowReviews !== undefined) safeData.allowReviews = dto.allowReviews;
+    if (dto.canRespondToReviews !== undefined) safeData.canRespondToReviews = dto.canRespondToReviews;
+    if (dto.description !== undefined) safeData.description = dto.description;
+    
     return this.prisma.drivingSchool.update({
       where: { id },
-      data: dto,
+      data: safeData,
     });
   }
 
